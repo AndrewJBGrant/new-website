35787e0443cc56cea81a098c0cdf354f65bd40d5
Andrew Grant <andrewgrant2009@hotmail.com>
2023-11-08 10:41:36 +0100

    working on header and image shape-outside

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,53 @@
-import Image from 'next/image';
-import data from './data.json';
-import LinkCard from './LInkCard';
+import Image from "next/image";
+import data from "./data.json";
+import LinkCard from "./LInkCard";
 
 export default function Home() {
-
-return(
-<div className="flex items-center flex-col mx-auto w-full justify-center mt-16 px-8">
-<Image className="rounded-full"
-alt={data.name}
-src={data.avatar}
-width={120}
-height={120}
-/>
-
-<h1 className="font-bold mt-4 mb-8 text-xl">{data.name}</h1>
-
-<section className="flex">
-{data.links.map((link) => (
-<LinkCard key={link.href}  {...link}/>
-))}
-
-{data.CV.map((cv) => (
-<LinkCard key={cv.href} download={"Download Andrew Grant CV"} {...cv}/>
-))}
-</section>
+  return (
+    <>
+      <div className="flex flex-col mx-auto w-full justify-center mt-16 px-8">
 
 
-</div>
-)
+        <article>
+        <Image
+          className="float-left aspect-[1/1] lg:aspect-[1/2]
+    rounded-full mb-0 mr-6
+
+    [shape-outside:circle(70%_at_20%_30%)]
+   
+    md:[shape-outside:polygon(0%_0%,100%_0%,75%_100%,0%_100%)]"
+          alt={data.name}
+          src={data.avatar}
+          width={120}
+          height={120}
+        />
+
+          <p className="">{data.heading}</p>
+          <h1 className="font-bold text-xl">{data.name}</h1>
+
+          <p>{data.intro}</p>
+        </article>
+
+
+
+
+
+
+      {/* // Links */}
+      <section className="flex justify-center mx-auto">
+        {data.links.map((link) => (
+          <LinkCard key={link.href} {...link} />
+        ))}
+
+        {data.CV.map((cv) => (
+          <LinkCard
+            key={cv.href}
+            download={"Download Andrew Grant CV"}
+            {...cv}
+          />
+        ))}
+      </section>
+      </div>
+    </>
+  );
 }
